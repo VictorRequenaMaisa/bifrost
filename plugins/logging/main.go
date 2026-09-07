06171c8511933211ae6aba4c4efd752a1649ad93
akshaydeo <akshay@akshaydeo.com>
2026-09-08 00:49:32 +0530

    Attribute endpoint MCP inspections in the standard logging pipeline

--- plugins/logging/main.go
+++ plugins/logging/main.go
@@ -2766,6 +2766,7 @@ func (p *LoggerPlugin) PreMCPHook(ctx *schemas.BifrostContext, req *schemas.Bifr
 	// Capture configured logging headers and x-bf-lh-* headers into metadata
 	entry.MetadataParsed = p.captureLoggingHeaders(ctx)
 
+	applyMCPObservation(ctx, entry)
 	p.pendingMCPLogsToInject.Store(mcpLogID, entry)
 
 	p.mu.Lock()
@@ -2910,6 +2911,7 @@ func (p *LoggerPlugin) PostMCPHook(ctx *schemas.BifrostContext, resp *schemas.Bi
 	p.mu.Unlock()
 	attachMCPLogRedactionData(ctx, entry, p.contentLoggingEnabled(ctx))
 	entry.PluginLogs = serializePluginLogs(ctx.GetPluginLogs())
+	applyMCPObservation(ctx, entry)
 	p.enqueueMCPToolLogEntry(entry, callback)
 
 	return resp, bifrostErr, nil
